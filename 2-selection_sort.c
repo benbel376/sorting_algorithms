2cc088f1e21845f9a27f5e44e08e702e291ae001
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-10-30 23:45:52 +0300

    selection sort commit 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,19 +11,25 @@ void selection_sort(int *array, size_t size)
 int temp;
 unsigned int i;
 unsigned int j;
-
+int exchanged = 0;
 
 for (i = 0; i < size; i++)
-{
-for (j = i; j < size; j++)
-{
-if (array[i] > array[j])
-{
-temp = array[i];
-array[i] = array[j];
-array[j] = temp;
- print_array(array, size);
+  {
+  for (j = i; j < size; j++)
+    {
+      if (array[i] > array[j])
+	{
+	  temp = array[i];
+	  array[i] = array[j];
+	  array[j] = temp;
+	  exchanged = 1;
+	}
+    }
+  if (exchanged == 1)
+    {
+      print_array(array, size);
+      exchanged = 0;
+    }
+   }
 }
-}
-}
-}
+
