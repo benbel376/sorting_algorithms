c1cf0ad488a93ed2e17d7a7bb93e439b0a5fcd60
Biniyam <44437166+benbel376@users.noreply.github.com>
2021-10-30 12:23:29 +0300

    Commit one

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,39 +1,29 @@
 #include "sort.h"
-
+#include <stdlib.h>
+#include <studio.h>
 /**
-* selection_sort - an implementating selection sort algorithm
-* @array: the array to be sorted
-* @size:size of the array
+ * selection_sort - implementer function
+ * @array: input array
+ * @size: size of input array
  */
 void selection_sort(int *array, size_t size)
 {
-unsigned int i = 0, j = 0, tmp_loc = 0;
-int *a;
-int *b;
-int tmp;
-int tmp1;
-while (i < size)
-{
-j = i;
-tmp = array[j], tmp_loc = j;
+int temp;
+unsigned int i;
+unsigned int j;
 
-while (j < size)
-{
-if (array[j] < tmp)
-tmp = array[j];
-tmp_loc = j;
-j++;
-}
 
-if (array[i] != array[tmp_loc])
+for (i = 0; i < size; i++)
 {
-a = array + i;
-b = array + tmp_loc;
-tmp1 = *a;
-*a = *b;
-*b = tmp1;
-print_array(array, size);
+temp = array[i];
+for (j = i; j < size; j++)
+{
+if (array[i] > array[j])
+{
+temp = array[i]
+array[i] = array[j]
+array[j] = temp
 }
-i++;
+}
 }
 }
